b3c99b12ba73aad0c8b774ae2dda2837bd5d5503
Brian Whisler <brian.whisler@gmail.com>
2023-09-11 09:30:37 -0600

    Update readme and ensure proper db seeding.

--- README.md
+++ README.md
@@ -3,23 +3,44 @@
 [![License](https://img.shields.io/github/license/shindekokoro/backroom-shopper)](http://choosealicense.com/licenses/mit/)
 
 ## Description
+
 The Backroom Shopper, is a backend for your e-commerce needs that utilizes express front end routing and a mysql database.
+MySQL and NODE required for usage.
 
 ## Table of Contents
+
 - [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
 - [License](#license)
 - [Tests](#tests)
 - [Questions](#questions)
 
+## Installation
+
+1. Clone repository `git clone https://github.com/shindekokoro/backroom-shopper.git`
+2. Type `cd backroom-shopper`
+3. Run `npm i` to install needed package dependencies.
+4. Rename `.env.EXAMPLE` to `.env` making the necessary changes.
+
+## Usage
+
+1. Type `npm run seed` to ensure database is properly created.
+   - `npm run seed data` if you want to seed sample data to database.
+2. Type `npm run start` to launch backroom-shopper API.
+
 ## License
+
 [MIT](http://choosealicense.com/licenses/mit/)
 
 A short and simple permissive license with conditions only requiring preservation of copyright and license notices. Licensed works, modifications, and larger works may be distributed under different terms and without source code.
 
 ## Tests
+
 macOS
 
 ## Questions
+
 **If you have any questions feel free to use the links below:**
 
 GitHub Profile: https://github.com/shindekokoro
--- seeds/index.js
+++ seeds/index.js
@@ -1,26 +1,47 @@
+require('dotenv').config();
+const sequelize = require('../config/connection');
+const mysql = require('mysql2/promise');
+
 const seedCategories = require('./category-seeds');
 const seedProducts = require('./product-seeds');
 const seedTags = require('./tag-seeds');
 const seedProductTags = require('./product-tag-seeds');
 
-const sequelize = require('../config/connection');
-
 const seedAll = async () => {
   await sequelize.sync({ force: true });
-  console.log('\n----- DATABASE SYNCED -----\n');
-  await seedCategories();
-  console.log('\n----- CATEGORIES SEEDED -----\n');
+  if (process.argv[2] && process.argv[2] === 'data') {
+    console.log('\n----- DATABASE SYNCED -----\n');
+    await seedCategories();
+    console.log('\n----- CATEGORIES SEEDED -----\n');
 
-  await seedProducts();
-  console.log('\n----- PRODUCTS SEEDED -----\n');
+    await seedProducts();
+    console.log('\n----- PRODUCTS SEEDED -----\n');
 
-  await seedTags();
-  console.log('\n----- TAGS SEEDED -----\n');
+    await seedTags();
+    console.log('\n----- TAGS SEEDED -----\n');
 
-  await seedProductTags();
-  console.log('\n----- PRODUCT TAGS SEEDED -----\n');
+    await seedProductTags();
+    console.log('\n----- PRODUCT TAGS SEEDED -----\n');
+  }
 
   process.exit(0);
 };
 
-seedAll();
+// If JAWSDB assume DB already created and source, otherwise check if database exists.
+if (process.env.JAWSDB_URL) {
+  return seedAll();
+} else {
+  return mysql
+    .createConnection({
+      user: process.env.DB_USER,
+      password: process.env.DB_PW
+    })
+    .then((connection) => {
+      connection
+        .query(`CREATE DATABASE IF NOT EXISTS ${process.env.DB_NAME};`)
+        .then(() => {
+          console.log('Database checked for existence.');
+          return seedAll();
+        });
+    });
+}
