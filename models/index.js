30d74b82355392d60a96bdbc2bf867d009804be5
Brian Whisler <brian.whisler@gmail.com>
2023-09-10 20:20:46 -0600

    reference foreignKey for ProductTag

--- models/index.js
+++ models/index.js
@@ -16,10 +16,14 @@ Category.hasMany(Product, {
 });
 
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(Tag, { through: ProductTag });
+Product.belongsToMany(Tag, {
+  through: { model: ProductTag, foreignKey: 'product_id' }
+});
 
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(Product, { through: ProductTag });
+Tag.belongsToMany(Product, {
+  through: { model: ProductTag, foreignKey: 'tag_id' }
+});
 
 module.exports = {
   Product,
